05d9ea8bea579d64610fc58b961181410d862eef
DavveDP <davve146@gmail.com>
2022-10-14 12:54:37 +0200

    Knock off + Teleport done

--- armips/move/battle_eff_seq/153.s
+++ armips/move/battle_eff_seq/153.s
@@ -14,6 +14,20 @@ a030_153:
     changevar VAR_OP_SET, VAR_ADD_STATUS1, 0x20000065
     endscript
 _0028:
+    tryswitchinmon BATTLER_ATTACKER, 0x1, _failed
+    gotosubscript 76
+    trynaturalcure BATTLER_ATTACKER, _0038
+    changemondatabyvalue VAR_OP_SET, BATTLER_ATTACKER, 0x34, 0x0
+_0038:
+    deletepokemon BATTLER_ATTACKER
+    waitmessage
+    preparehpgaugeslide BATTLER_ATTACKER
+    waitmessage
+    changevar VAR_OP_SETMASK, VAR_60, 0x10
+    changevar VAR_OP_CLEARMASK, VAR_06, 0x80
+    changevar VAR_OP_SET, VAR_47, 0x0
+    jumptosubseq 10
+_failed:
     changevar VAR_OP_SETMASK, VAR_10, 0x40
     endscript
 
--- armips/move/battle_eff_seq/188.s
+++ armips/move/battle_eff_seq/188.s
@@ -10,7 +10,14 @@
 .create "build/move/battle_eff_seq/0_188", 0
 
 a030_188:
-    changevar VAR_OP_SET, VAR_ADD_STATUS2, 0x20000036
+    ifmonstat IF_NOTEQUAL, BATTLER_DEFENDER, MON_DATA_ITEM, 0x0, _hasItem
+    changevar VAR_OP_SET, VAR_ADD_STATUS2, 0x20000036 
+    critcalc
+    damagecalc
+    endscript
+_hasItem:
+    changevar VAR_OP_SET, VAR_DAMAGE_MULT, 0x14
+    changevar VAR_OP_SET, VAR_ADD_STATUS2, 0x20000036 
     critcalc
     damagecalc
     endscript
