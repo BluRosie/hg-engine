4ddc1f18bb57f8d62f981e49fab98f42e68eab82
turtleisaac <turtleisaac1@gmail.com>
2023-03-21 12:40:53 -0500

    put back hacky solution to linking functions into the armips files; added offset prints for debug in bdhcam.s

--- armips/asm/bdhcam.s
+++ armips/asm/bdhcam.s
@@ -3,7 +3,14 @@
 .nds
 .thumb
 
+.include "armips/asm/offset.s"
+
 .open "base/arm9.bin", 0x02000000
+
+.notice  "FUN_GDMI: 0x" + toHex(FUN_GDMI, 8)
+.notice  "FUN_RHDtaArc: 0x" + toHex(FUN_RHDtaArc, 8)
+.notice  "FUN_ReadEntryDataArc: 0x" + toHex(FUN_ReadEntryDataArc, 8)
+
 
 .org 0x02023174
     bl FUN_GDMI
@@ -15,7 +22,6 @@
 .org 0x21FB04C
     .byte 0x00, 0x4B, 0x18, 0x47
     .word FUN_RHDtaArc
-    .notice  "Message"
 
 .org 0x21FB164
     .byte 0x00, 0x4B, 0x18, 0x47
