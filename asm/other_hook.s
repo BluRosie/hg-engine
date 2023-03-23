3f0125ed4f00df855f3aea3c41188072da93d050
turtleisaac <turtleisaac1@gmail.com>
2023-03-22 19:50:18 -0500

    put missing WindowClose

--- asm/other_hook.s
+++ asm/other_hook.s
@@ -297,3 +297,15 @@ bx r2
 .global gFieldSysPtr
 gFieldSysPtr:
 .word 0
+
+.global WindowClose
+WindowClose:
+ldr r0, =0x04000050
+mov r1, #0x0
+strh r1, [r0]
+mov r0, r5
+add r0, #0xD2
+ldrb r1, [r0, #0x0]
+mov r0, #0x40
+ldr r2, =0x02041198 + 1
+bx r2
