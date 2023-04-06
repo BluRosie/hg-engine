66c17f20dacbfb212609c68165b7e88ce9904602
Nathan Wichers <bluroseai@hotmail.com>
2023-04-06 17:21:51 -0400

    fix all the wavs for ntrWavTool

--- data/graphics/overworlds/copyOW2Folder.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-set /p input=  OW Name to grab:  
-set /p input2=  Target OW name:  
-echo "Copying %input% to %input2%."
-cp C:\Users\quinc\Downloads\work\overworlds\normal\%input%.png D:\heartgold\hg-engine\data\graphics\overworlds\%input2%.png
-cp C:\Users\quinc\Downloads\work\overworlds\shiny\%input%.png D:\heartgold\hg-engine\data\graphics\overworlds\%input2%_shiny.png
