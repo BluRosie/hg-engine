bd1890496a7c7795a602372d3c3552f1c37104f7
PandaPanda-Panda <jakeison@outlook.com>
2023-06-08 15:00:44 +0100

    correcting new move eff

--- armips/move/battle_eff_seq/293.s
+++ armips/move/battle_eff_seq/293.s
@@ -7,12 +7,12 @@
 .include "armips/include/monnums.s"
 .include "armips/include/movenums.s"
 
-.create "build/move/battle_eff_seq/0_045", 0
+.create "build/move/battle_eff_seq/0_293", 0
 
-a030_045:
-    abilitycheck 0x1, BATTLER_ATTACKER, ABILITY_RECKLESS, _0024
+a030_293:
+    abilitycheck 0x1, BATTLER_ATTACKER, ABILITY_RECKLESS, a045_293
     changevar VAR_OP_SET, VAR_DAMAGE_MULT, 0xC
-_0024:
+a045_293:
     changevar VAR_OP_SETMASK, VAR_06, 0x1000
     changevar VAR_OP_SET, VAR_ADD_STATUS1, 0x80000007
     critcalc
