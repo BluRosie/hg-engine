c0b11a6bdb4daa3969dda0771e6e42e9080a6f7a
Brioooche <67444248+Brioooche@users.noreply.github.com>
2023-06-14 18:14:18 -0300

    Start Snow (Hail) Gen IX mechanics
    We'll decide what to do with Hail after, for now add the defense increase to ice pokémon

--- src/battle/battle_calc_damage.c
+++ src/battle/battle_calc_damage.c
@@ -495,8 +495,6 @@ int CalcBaseDamage(void *bw, struct BattleStruct *sp, int moveno, u32 side_cond,
         defense *= 2;
     }
 
-
-
     // handle mud/water sport
     if ((movetype == TYPE_ELECTRIC) && (CheckFieldMoveEffect(bw, sp, MOVE_EFFECT_FLAG_MUD_SPORT)))
     {
@@ -722,6 +720,11 @@ int CalcBaseDamage(void *bw, struct BattleStruct *sp, int moveno, u32 side_cond,
         {
             sp_defense = sp_defense * 15 / 10;
         }
+        if ((field_cond & WEATHER_HAIL_ANY) &&
+            ((DefendingMon.type1 == TYPE_ICE) || (DefendingMon.type2 == TYPE_ICE)))
+        {
+            defense = defense * 15 / 10;
+        }		
         if ((field_cond & WEATHER_SUNNY_ANY) &&
             (CheckSideAbility(bw, sp, CHECK_PLAYER_SIDE_ALIVE, attacker, ABILITY_FLOWER_GIFT)))
         {
