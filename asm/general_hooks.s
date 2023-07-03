5aefbfa14ccf78362d2d91f9b2dcc2cfe1b85b14
Nathan Wichers <bluroseai@hotmail.com>
2023-07-03 11:44:26 -0400

    add rare candy level cap

--- asm/general_hooks.s
+++ asm/general_hooks.s
@@ -279,7 +279,7 @@ bl bx_r3
 // r0 is level
 bl IsLevelAtLevelCap
 cmp r0, #1
-beq return_to_2245B3E // below level cap
+beq return_to_2245B3E // above level cap
 ldr r3, =0x02245A32 | 1
 bx r3
 
@@ -291,3 +291,21 @@ bx_r3:
 bx r3
 
 .pool
+
+
+// r0 is level
+.global CapRareCandies
+CapRareCandies:
+bl IsLevelAtLevelCap
+cmp r0, #0
+bne return_to_208FEFA
+mov r0, r4
+ldr r3, =0x0201AB0C | 1 // free memory
+bl bx_r3
+ldr r3, =0x0208FEF4 | 1
+bx r3
+
+return_to_208FEFA:
+ldr r3, =0x0208FEFA | 1
+bx r3
+
