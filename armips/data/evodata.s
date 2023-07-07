bb7bbddd75129c32ddecdef1cf13496776fe5f9a
Brioooche <67444248+Brioooche@users.noreply.github.com>
2023-07-06 23:52:31 -0300

    Fix Stantler evodata

--- armips/data/evodata.s
+++ armips/data/evodata.s
@@ -2819,7 +2819,7 @@ evodata SPECIES_PORYGON2
     terminateevodata
 
 evodata SPECIES_STANTLER
-    evolution EVO_KNOWS_MOVE, MOVE_PSYSHIELD_BASH, SPECIES_WYRDEER \\placeholder
+    evolution EVO_KNOWS_MOVE, MOVE_PSYSHIELD_BASH, SPECIES_WYRDEER //placeholder
     evolution EVO_NONE, 0, SPECIES_NONE
     evolution EVO_NONE, 0, SPECIES_NONE
     evolution EVO_NONE, 0, SPECIES_NONE
