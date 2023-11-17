bdaac2fb8ab2ca9917012fedbf168415d58994a8
Mixone-FinallyHere <miguel.terol.93@gmail.com>
2023-11-18 00:19:27 +0100

    Small script for auto setting mondata dex entries

---
+++ scripts/fix_text.py
@@ -0,0 +1,30 @@
+import os
+
+def doIt():
+    file = "../armips/data/mondata.s"
+    with open(file, "r") as infile:
+        data = infile.readlines()
+    with open("fixed/mondata.s", "w") as outfile:
+        newData = []
+        for line in data:
+            if "mondexentry" in line:
+                newline = line.split('"')
+                dexData = newline[-2]
+                if len(dexData) > 1:
+                    count = 0
+                    newText = ""
+                    for word in dexData.replace("\\n", " ").split():
+                        if count + len(word) < 40:
+                            if count != 0:
+                                newText += " "
+                                count += 1
+                            newText += word
+                            count += len(word)
+                        else:
+                            newText += "\\n" + word
+                            count = len(word)
+                                
+                    newline[-2] = '"' + newText + '"'
+                    line = " ".join(newline)
+            newData.append(line)
+        outfile.write("".join(newData))
