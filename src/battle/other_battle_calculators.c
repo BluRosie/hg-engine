48999fe5b176eae8cbae3e989f3a8fee09e79491
pkrayzy <pkrzystek@outlook.com>
2023-12-20 21:52:06 -0500

    define crit

--- src/battle/other_battle_calculators.c
+++ src/battle/other_battle_calculators.c
@@ -771,6 +771,7 @@ int CalcCritical(void *bw, struct BattleStruct *sp, int attacker, int defender, 
 {
     // u16 temp;
     u16 critUp;
+    u16 CritChance;
     u16 item;
     int hold_effect;
     // u16 speed;
