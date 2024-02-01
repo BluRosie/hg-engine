a66c0ac2daf790bd346c6b4d9819a1c83336540a
Nathan Wichers <bluroseai@hotmail.com>
2024-01-31 20:36:26 -0500

    move party creation routines to field

--- asm/general_hooks.s
+++ asm/general_hooks.s
@@ -206,23 +206,6 @@ GiveMon_return_address:
 .word 0
 
 
-.global AddWildPartyPokemon_hook
-AddWildPartyPokemon_hook:
-ldr r5, =AddWildPartyPokemon_return_address
-mov r6, lr
-str r6, [r5]
-pop {r5-r6}
-bl AddWildPartyPokemon
-ldr r1, =AddWildPartyPokemon_return_address
-ldr r1, [r1]
-mov pc, r1
-
-.pool
-
-AddWildPartyPokemon_return_address:
-.word 0
-
-
 .global CreateBoxMonData_hook
 CreateBoxMonData_hook:
 ldr r5, =CreateBoxMonData_return_address
