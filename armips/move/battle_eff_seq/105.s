f2da2e6d1be74a8975189e6bfc44b9ea5b4399c1
Aero <47219799+Aeliko@users.noreply.github.com>
2024-02-02 22:32:13 +0800

    Implement Thief, Covet, Bug Bite, Pluck interaction

--- armips/move/battle_eff_seq/105.s
+++ armips/move/battle_eff_seq/105.s
@@ -7,10 +7,14 @@
 .include "armips/include/monnums.s"
 .include "armips/include/movenums.s"
 
+// Thief, Covet
+
 .create "build/move/battle_eff_seq/0_105", 0
 
 a030_105:
+    iffirsthitofparentalbond SkipEffect
     changevar VAR_OP_SET, VAR_ADD_STATUS2, 0x8100001F
+SkipEffect:
     critcalc
     damagecalc
     endscript
--- armips/move/battle_eff_seq/224.s
+++ armips/move/battle_eff_seq/224.s
@@ -7,10 +7,14 @@
 .include "armips/include/monnums.s"
 .include "armips/include/movenums.s"
 
+// Bug Bite, Pluck
+
 .create "build/move/battle_eff_seq/0_224", 0
 
 a030_224:
+    iffirsthitofparentalbond SkipEffect
     changevar VAR_OP_SET, VAR_ADD_STATUS2, 0x20000087
+SkipEffect:
     critcalc
     damagecalc
     endscript
