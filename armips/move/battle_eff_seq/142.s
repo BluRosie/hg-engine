9a098202580f63d86e95be77469d3ef9f2136ef0
Koekenpan <koekenpannosrs@gmail.com>
2024-02-27 21:25:31 +0100

    Implement Shed Tail

--- armips/move/battle_eff_seq/142.s
+++ armips/move/battle_eff_seq/142.s
@@ -10,6 +10,10 @@
 .create "build/move/battle_eff_seq/0_142", 0
 
 a030_142:
+    if IF_NOTEQUAL, VAR_CURRENT_MOVE, MOVE_SHED_TAIL, _BellyDrum 
+    changevar VAR_OP_SET, VAR_ADD_STATUS1, ADD_STATUS_WORK | ADD_STATUS_SHED_TAIL
+    endscript
+_BellyDrum:
     changevar VAR_OP_SET, VAR_ADD_STATUS1, 0x20000064
     endscript
 
