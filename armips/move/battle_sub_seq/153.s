2619240c796764d40dc9bbe1f62325f12614ef78
Nathan Wichers <bluroseai@hotmail.com>
2024-05-09 13:38:13 -0400

    fix acid rain glitch
    also fix typo in overlay table

--- armips/move/battle_sub_seq/153.s
+++ armips/move/battle_sub_seq/153.s
@@ -2,6 +2,7 @@
 .thumb
 
 .include "armips/include/battlescriptcmd.s"
+.include "armips/include/battle_script_constants.s"
 .include "armips/include/abilities.s"
 .include "armips/include/itemnums.s"
 .include "armips/include/monnums.s"
@@ -10,7 +11,7 @@
 .create "build/move/battle_sub_seq/1_153", 0
 
 a001_153:
-    gotosubscript 279
+    gotosubscript SUB_SEQ_PUSH_ATTACKER_DEFENDER
 _0008:
     trypursuit _03BC
     changevar VAR_OP_SET, VAR_DAMAGE_MULT, 0x14
@@ -38,19 +39,19 @@ _00F4:
     changevar2 VAR_OP_SET, VAR_DEFENDER_DAMAGE, VAR_DAMAGE
     changevar2 VAR_OP_SET, VAR_BATTLER_SOMETHING, VAR_DEFENDER
     checkshouldleavewith1hp BATTLER_xFF
-    gotosubscript 2
-    gotosubscript 16
-    gotosubscript 21
+    gotosubscript SUB_SEQ_HP_CHANGE
+    gotosubscript SUB_SEQ_CRITICAL_HIT
+    gotosubscript SUB_SEQ_MOVE_SUCCESS_MESSAGE
     ifmonstat IF_EQUAL, BATTLER_DEFENDER, MON_DATA_HP, 0x0, _029C
     abilityeffectcheckonhit _016C
-    gotosubscript2 43
+    gotosubscript2 VAR_TEMP_WORK
 _016C:
     itemeffectcheckonhit _017C
-    gotosubscript2 43
+    gotosubscript2 VAR_TEMP_WORK
 _017C:
     changevar VAR_OP_SETMASK, VAR_SERVER_STATUS1, 0x2000
     checkuturnitemeffect _019C
-    gotosubscript2 43
+    gotosubscript2 VAR_TEMP_WORK
 _019C:
     changevar VAR_OP_CLEARMASK, VAR_SERVER_STATUS1, 0x2000
     goto _027C
@@ -66,28 +67,28 @@ _0204:
     changevar VAR_OP_MUL, VAR_SHELL_BELL_DAMAGE, 0xFFFFFFFF
 _023C:
     changevar2 VAR_OP_SET, VAR_BATTLER_SOMETHING, VAR_DEFENDER
-    gotosubscript 90
-    gotosubscript 16
-    gotosubscript 21
+    gotosubscript SUB_SEQ_HIT_SUBSTITUTE
+    gotosubscript SUB_SEQ_CRITICAL_HIT
+    gotosubscript SUB_SEQ_MOVE_SUCCESS_MESSAGE
     goto _027C
 _026C:
     wait 0xF
-    gotosubscript 7
+    gotosubscript SUB_SEQ_MOVE_FAIL
 _027C:
-    gotosubscript 280
+    gotosubscript SUB_SEQ_POP_ATTACKER_DEFENDER
     changevar2 VAR_OP_GET_RESULT, VAR_MOVE_TEMP, VAR_CURRENT_MOVE
     goto _0008
 _029C:
-    gotosubscript 83
+    gotosubscript SUB_SEQ_DESTINY_BOND_FAINT_FOE
     abilityeffectcheckonhit _02B4
-    gotosubscript2 43
+    gotosubscript2 VAR_TEMP_WORK
 _02B4:
     itemeffectcheckonhit _02C4
-    gotosubscript2 43
+    gotosubscript2 VAR_TEMP_WORK
 _02C4:
     changevar VAR_OP_SETMASK, VAR_SERVER_STATUS1, 0x2000
     checkuturnitemeffect _02E4
-    gotosubscript2 43
+    gotosubscript2 VAR_TEMP_WORK
 _02E4:
     changevar VAR_OP_CLEARMASK, VAR_SERVER_STATUS1, 0x2000
     changevar2 VAR_OP_SET, VAR_TEMP_WORK, VAR_FAINTED_BATTLER
@@ -97,13 +98,14 @@ _02E4:
     changevar VAR_OP_CLEARMASK, VAR_SERVER_STATUS2, 0xF0000000
 _0344:
     if IF_NOTMASK, VAR_CALCULATION_WORK, 0x1, _0360
-    gotosubscript 276
+    gotosubscript SUB_SEQ_GET_EXP
 _0360:
     changevar VAR_OP_ADD, VAR_FAINTED_BATTLER, 0x1
     changevar VAR_OP_RSH, VAR_CALCULATION_WORK, 0x1
     if IF_NOTEQUAL, VAR_CALCULATION_WORK, 0x0, _0344
-    changevar2 VAR_OP_SUB_TO_ZERO, VAR_FIELD_EFFECT, VAR_TEMP_WORK
-    gotosubscript 280
+// fix acid rain glitch
+    changevar2 VAR_OP_SET, VAR_FAINTED_BATTLER, VAR_TEMP_WORK
+    gotosubscript SUB_SEQ_POP_ATTACKER_DEFENDER
     changevar2 VAR_OP_GET_RESULT, VAR_MOVE_TEMP, VAR_CURRENT_MOVE
 _03BC:
     endscript
