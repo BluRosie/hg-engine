c6371025e22dbd2615b48b8508ab7e42538aee60
alangston24 <97756191+alangston24@users.noreply.github.com>
2024-07-05 10:58:40 -0500

    Update starters.s
    Changed starters

--- armips/data/starters.s
+++ armips/data/starters.s
@@ -9,9 +9,9 @@
 // sequence. To update that text, modify text archive 190 in
 // DSPRE.
 .macro STARTER_CHOICES
-    .word SPECIES_CHIKORITA
-    .word SPECIES_CYNDAQUIL
-    .word SPECIES_TOTODILE
+    .word SPECIES_SNIVY
+    .word SPECIES_CHIMCHAR
+    .word SPECIES_FROAKIE
 .endmacro
 
 .open "base/arm9.bin", 0x02000000
