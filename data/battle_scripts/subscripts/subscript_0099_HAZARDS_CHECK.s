d39c77d6096048d2e5db3976deab1d85c8c19bf2
Koekenpan <149670265+Koekenpann@users.noreply.github.com>
2024-10-27 09:55:49 +0100

    Reorder Spikes and Toxic Spikes

--- data/battle_scripts/subscripts/subscript_0099_HAZARDS_CHECK.s
+++ data/battle_scripts/subscripts/subscript_0099_HAZARDS_CHECK.s
@@ -25,41 +25,18 @@ _printUnnerveMessage:
 _realHazardsCheck:
     // toxic spikes can inflict poison through magic guard, but not subsequently damage the mon (which is already handled in the damage subscript)
     //CheckAbility CHECK_OPCODE_HAVE, BATTLER_CATEGORY_SWITCHED_MON, ABILITY_MAGIC_GUARD, _105
-    CompareVarToValue OPCODE_FLAG_SET, BSCRIPT_VAR_FIELD_CONDITION, FIELD_CONDITION_GRAVITY, _checkToxicSpikes
-    CheckItemHoldEffect CHECK_OPCODE_HAVE, BATTLER_CATEGORY_SWITCHED_MON, HOLD_EFFECT_SPEED_DOWN_GROUNDED, _checkToxicSpikes
+    CompareVarToValue OPCODE_FLAG_SET, BSCRIPT_VAR_FIELD_CONDITION, FIELD_CONDITION_GRAVITY, _checkSpikes
+    CheckItemHoldEffect CHECK_OPCODE_HAVE, BATTLER_CATEGORY_SWITCHED_MON, HOLD_EFFECT_SPEED_DOWN_GROUNDED, _checkSpikes
     CheckAbility CHECK_OPCODE_HAVE, BATTLER_CATEGORY_SWITCHED_MON, ABILITY_LEVITATE, _checkStealthRock
     CompareMonDataToValue OPCODE_EQU, BATTLER_CATEGORY_SWITCHED_MON, BMON_DATA_TYPE_1, TYPE_FLYING, _checkStealthRock
     CompareMonDataToValue OPCODE_EQU, BATTLER_CATEGORY_SWITCHED_MON, BMON_DATA_TYPE_2, TYPE_FLYING, _checkStealthRock
     CompareMonDataToValue OPCODE_FLAG_SET, BATTLER_CATEGORY_SWITCHED_MON, BMON_DATA_MOVE_EFFECT, MOVE_EFFECT_FLAG_MAGNET_RISE, _checkStealthRock
     CheckItemHoldEffect CHECK_OPCODE_HAVE, BATTLER_CATEGORY_SWITCHED_MON, HOLD_EFFECT_UNGROUND_DESTROYED_ON_HIT, _checkStealthRock
 
-_checkToxicSpikes:
-    CheckToxicSpikes BATTLER_CATEGORY_SWITCHED_MON, _checkSpikes
-    CompareVarToValue OPCODE_EQU, BSCRIPT_VAR_CALC_TEMP, 0x00000002, _badlyPoison
-    CompareVarToValue OPCODE_EQU, BSCRIPT_VAR_CALC_TEMP, 0x00000001, _regularPoison
-    // The poison spikes disappeared from around your team’s feet!
-    PrintMessage 1065, TAG_NONE_SIDE, BATTLER_CATEGORY_SWITCHED_MON
-    Wait 
-    WaitButtonABTime 30
-    GoTo _checkSpikes
-
-_regularPoison:
-    // a mon with comatose can not be poisoned by toxic spikes
-    CheckAbility CHECK_OPCODE_HAVE, BATTLER_CATEGORY_SWITCHED_MON, ABILITY_COMATOSE, _checkSpikes
-    CheckItemHoldEffect CHECK_OPCODE_HAVE, BATTLER_CATEGORY_SWITCHED_MON, HOLD_EFFECT_IGNORE_ENTRY_HAZARDS, _end
-    Call BATTLE_SUBSCRIPT_POISON
-    GoTo _checkSpikes
-
-_badlyPoison:
-    // a mon with comatose can not be poisoned by toxic spikes
-    CheckAbility CHECK_OPCODE_HAVE, BATTLER_CATEGORY_SWITCHED_MON, ABILITY_COMATOSE, _checkSpikes
-    CheckItemHoldEffect CHECK_OPCODE_HAVE, BATTLER_CATEGORY_SWITCHED_MON, HOLD_EFFECT_IGNORE_ENTRY_HAZARDS, _end
-    Call BATTLE_SUBSCRIPT_BADLY_POISON
-
 _checkSpikes:
-    // ignore any further entry hazards with heavy duty boots
-    CheckItemHoldEffect CHECK_OPCODE_HAVE, BATTLER_CATEGORY_SWITCHED_MON, HOLD_EFFECT_IGNORE_ENTRY_HAZARDS, _end
-    CheckSpikes BATTLER_CATEGORY_SWITCHED_MON, _checkStickyWeb
+    // Skip spikes damage if HDB, but still check TSpikes for grounded Poison types to clear it.
+    CheckItemHoldEffect CHECK_OPCODE_HAVE, BATTLER_CATEGORY_SWITCHED_MON, HOLD_EFFECT_IGNORE_ENTRY_HAZARDS, _checkToxicSpikes
+    CheckSpikes BATTLER_CATEGORY_SWITCHED_MON, _checkToxicSpikes
     UpdateVarFromVar OPCODE_SET, BSCRIPT_VAR_MSG_BATTLER_TEMP, BSCRIPT_VAR_BATTLER_SWITCH
     UpdateVar OPCODE_FLAG_ON, BSCRIPT_VAR_BATTLE_STATUS, BATTLE_STATUS_NO_BLINK
     Call BATTLE_SUBSCRIPT_UPDATE_HP
@@ -67,6 +44,31 @@ _checkSpikes:
     PrintMessage 429, TAG_NICKNAME, BATTLER_CATEGORY_SWITCHED_MON
     Wait 
     WaitButtonABTime 30
+
+_checkToxicSpikes:
+    CheckToxicSpikes BATTLER_CATEGORY_SWITCHED_MON, _checkStickyWeb
+    CompareVarToValue OPCODE_EQU, BSCRIPT_VAR_CALC_TEMP, 0x00000002, _badlyPoison
+    CompareVarToValue OPCODE_EQU, BSCRIPT_VAR_CALC_TEMP, 0x00000001, _regularPoison
+    // The poison spikes disappeared from around your team’s feet!
+    PrintMessage 1065, TAG_NONE_SIDE, BATTLER_CATEGORY_SWITCHED_MON
+    Wait 
+    WaitButtonABTime 30
+    GoTo _checkStickyWeb
+
+_regularPoison:
+    // a mon with comatose can not be poisoned by toxic spikes
+    CheckAbility CHECK_OPCODE_HAVE, BATTLER_CATEGORY_SWITCHED_MON, ABILITY_COMATOSE, _checkStickyWeb
+    CheckItemHoldEffect CHECK_OPCODE_HAVE, BATTLER_CATEGORY_SWITCHED_MON, HOLD_EFFECT_IGNORE_ENTRY_HAZARDS, _end
+    Call BATTLE_SUBSCRIPT_POISON
+    GoTo _checkStickyWeb
+
+_badlyPoison:
+    // a mon with comatose can not be poisoned by toxic spikes
+    CheckAbility CHECK_OPCODE_HAVE, BATTLER_CATEGORY_SWITCHED_MON, ABILITY_COMATOSE, _checkStickyWeb
+    CheckItemHoldEffect CHECK_OPCODE_HAVE, BATTLER_CATEGORY_SWITCHED_MON, HOLD_EFFECT_IGNORE_ENTRY_HAZARDS, _end
+    Call BATTLE_SUBSCRIPT_BADLY_POISON
+
+// TODO: G-Max Steelsurge Hazard should be here for the correct order
 
 _checkStickyWeb:
     UpdateVarFromVar OPCODE_SET, BSCRIPT_VAR_BATTLER_ATTACKER, BSCRIPT_VAR_BATTLER_SWITCH
