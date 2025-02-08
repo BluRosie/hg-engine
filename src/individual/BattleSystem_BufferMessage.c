a91394ccdd6350cd6f09c293232796be470f54d4
Nathan Wichers <bluroseai@hotmail.com>
2025-02-08 15:10:14 -0500

    buffer the indefinite article for all messages identifed so far

--- src/individual/BattleSystem_BufferMessage.c
+++ src/individual/BattleSystem_BufferMessage.c
@@ -71,7 +71,7 @@ void BattleSystem_BufferMessage(struct BattleSystem *bsys, MESSAGE_PARAM *msg) {
         break;
     case TAG_NICKNAME_ITEM:
         BattleMessage_BufferNickname(bsys, 0, msg->msg_para[0]);
-        if (msg->msg_id == 513) {
+        if ((msg->msg_id - BATTLE_MSG_OBTAINED_ITEM) < 3) { // get article added to each of these
             BufferItemNameWithIndefArticle(bsys->msgFormat, 1, msg->msg_para[1]);
         } else {
             BattleMessage_BufferItem(bsys, 1, msg->msg_para[1]);
@@ -258,9 +258,15 @@ void BattleSystem_BufferMessage(struct BattleSystem *bsys, MESSAGE_PARAM *msg) {
         break;
     case TAG_NICKNAME_ITEM_NICKNAME_ITEM:
         BattleMessage_BufferNickname(bsys, 0, msg->msg_para[0]);
-        BattleMessage_BufferItem(bsys, 1, msg->msg_para[1]);
+        if ((msg->msg_id - BATTLE_MSG_OBTAINED_ITEM_OBTAINED_ITEM) < 7)
+            BufferItemNameWithIndefArticle(bsys->msgFormat, 1, msg->msg_para[1]);
+        else
+            BattleMessage_BufferItem(bsys, 1, msg->msg_para[1]);
         BattleMessage_BufferNickname(bsys, 2, msg->msg_para[2]);
-        BattleMessage_BufferItem(bsys, 3, msg->msg_para[3]);
+        if ((msg->msg_id - BATTLE_MSG_OBTAINED_ITEM_OBTAINED_ITEM) < 7)
+            BufferItemNameWithIndefArticle(bsys->msgFormat, 3, msg->msg_para[3]);
+        else
+            BattleMessage_BufferItem(bsys, 3, msg->msg_para[3]);
         break;
     case TAG_TRNAME_NICKNAME_TRNAME_NICKNAME:
         BattleMessage_BufferTrainerName(bsys, 0, msg->msg_para[0]);
