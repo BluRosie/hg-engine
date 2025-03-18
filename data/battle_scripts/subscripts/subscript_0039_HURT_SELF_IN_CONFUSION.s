ca7e8b33f6cca2d11623323b2e2e1adececd6be0
Nathan Wichers <bluroseai@hotmail.com>
2025-03-18 19:49:34 -0400

    disguise now activates on confusion damage

--- data/battle_scripts/subscripts/subscript_0039_HURT_SELF_IN_CONFUSION.s
+++ data/battle_scripts/subscripts/subscript_0039_HURT_SELF_IN_CONFUSION.s
@@ -14,9 +14,15 @@ _000:
     PrintMessage 797, TAG_NONE
     Wait 
     WaitButtonABTime 30
-    CalcConfusionDamage
+    CalcConfusionDamage _breakIceFace
     UnlockMoveChoice BATTLER_CATEGORY_ATTACKER
     CheckHoldOnWith1HP BATTLER_CATEGORY_ATTACKER
     Call BATTLE_SUBSCRIPT_UPDATE_HP
     Call BATTLE_SUBSCRIPT_MOVE_FOLLOWUP_MESSAGE
+    End 
+
+_breakIceFace:
+    UnlockMoveChoice BATTLER_CATEGORY_ATTACKER
+    Call BATTLE_SUBSCRIPT_MOVE_FOLLOWUP_MESSAGE
+    Call BATTLE_SUBSCRIPT_HANDLE_DISGUISE_ICE_FACE
     End 
