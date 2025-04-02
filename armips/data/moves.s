a6e22b49b1c189e607f508001a04f84e753cb26b
blackisme1 <mskim020815@gmail.com>
2025-04-02 20:51:20 +0900

    a

--- armips/data/moves.s
+++ armips/data/moves.s
@@ -6,7 +6,6 @@
 .include "armips/include/config.s"
 .include "armips/include/movemacros.s"
 
-.include "asm/include/debug.inc"
 .include "asm/include/moves.inc"
 .include "asm/include/move_effects.inc"
 
