ef9db958c8da47dc955ace7cfc230cc0532ee08d
Aero <47219799+Aeliko@users.noreply.github.com>
2025-07-09 12:50:00 +0800

    Fix #460

--- src/battle/battle_pokemon.c
+++ src/battle/battle_pokemon.c
@@ -1159,7 +1159,7 @@ u32 LONG_CALL GetAdjustedMoveTypeBasics(struct BattleStruct *sp, u32 move, u32 a
  */
 u32 LONG_CALL GetAdjustedMoveType(struct BattleStruct *sp, u32 client, u32 move)
 {
-    return GetAdjustedMoveTypeBasics(sp, move, GetBattlerAbility(sp, client), sp->move_type);
+    return GetAdjustedMoveTypeBasics(sp, move, GetBattlerAbility(sp, client), GetDynamicMoveType(gBattleSystem, sp, client, move));
 }
 
 /**
