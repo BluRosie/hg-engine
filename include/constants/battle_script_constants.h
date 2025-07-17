d7a29e1f0b1b837e663dd328a27a208772364184
kumakuma1 <kuehnmaxi@gmail.com>
2025-07-17 15:41:44 +0200

    - fix speed order sorting - implement gems - add beforeMove logic - script to print message, remove item - check if any battler is hit, trigger gem if so - itemdata updated for gems - multihit moves have all hits boosted - pledge moves can not trigger gems - TODO symbiosis

--- include/constants/battle_script_constants.h
+++ include/constants/battle_script_constants.h
@@ -458,8 +458,9 @@
 #define SUB_SEQ_HANDLE_WIND_RIDER                     (448)
 #define SUB_SEQ_HANDLE_CHARGE_BOOST                   (449)
 #define SUB_SEQ_SPIKY_SHIELD                          (450)
+#define SUB_SEQ_HANDLE_GEM_ACTIVATION_MESSAGE         (451)
 
-#define MAX_BASE_SUBSCRIPT_NUM 450
+#define MAX_BASE_SUBSCRIPT_NUM 451
 
 // define your custom subscripts below like this
 // #define MOVE_SUBSCRIPT_PTR_CUSTOM_1 (MAX_BASE_SUBSCRIPT_NUM + 1)
--- src/battle/other_battle_calculators.c
+++ src/battle/other_battle_calculators.c
@@ -3179,7 +3179,7 @@ void LONG_CALL SortRawSpeedNonRNGArray(struct BattleSystem *bsys, struct BattleS
 
     int rawSpeedArray[4] = {0, 0, 0, 0};
 
-    for (int i = 0; i < client_set_max - 1; i++) {
+    for (int i = 0; i < client_set_max; i++) {
         ctx->rawSpeedNonRNGClientOrder[i] = ctx->turnOrder[i];
 
         // Get the original unmodified speed stat from the party
@@ -3447,6 +3447,54 @@ BOOL LONG_CALL CanKnockOffApply(struct BattleSystem *bw, struct BattleStruct *sp
     if (item != 0 && CanItemBeRemovedFromClient(sp, defender))
     {
         return TRUE;
+    }
+    return FALSE;
+}
+
+
+
+BOOL LONG_CALL IsAnyBattleMonHit(struct BattleStruct* ctx)
+{
+    u8 i = 0;
+    if ((IS_TARGET_BOTH_MOVE(ctx) || IS_TARGET_FOES_AND_ALLY_MOVE(ctx)))
+    {
+        while (i <= SPREAD_MOVE_LOOP_MAX)
+        {
+            switch (i)
+            {
+            case SPREAD_MOVE_LOOP_ALLY:
+                i++;
+                if ((IS_TARGET_FOES_AND_ALLY_MOVE(ctx) || BATTLER_ALLY(ctx->attack_client) == ctx->defence_client)
+                    && IS_VALID_MOVE_TARGET(ctx, BATTLER_ALLY(ctx->attack_client)))
+                {
+                    return TRUE;
+                }
+                FALLTHROUGH;
+
+            case SPREAD_MOVE_LOOP_OPPONENT_LEFT:
+                i++;
+                if ((IS_TARGET_BOTH_MOVE(ctx) || IS_TARGET_FOES_AND_ALLY_MOVE(ctx))
+                    && IS_VALID_MOVE_TARGET(ctx, BATTLER_OPPONENT_SIDE_LEFT(ctx->attack_client)))
+                {
+                    return TRUE;
+                }
+                FALLTHROUGH;
+            case SPREAD_MOVE_LOOP_OPPONENT_RIGHT:
+                i++;
+                if ((IS_TARGET_BOTH_MOVE(ctx) || IS_TARGET_FOES_AND_ALLY_MOVE(ctx))\
+                    && IS_VALID_MOVE_TARGET(ctx, BATTLER_OPPONENT_SIDE_RIGHT(ctx->attack_client)))
+                {
+                    return TRUE;
+                }
+            }
+        }
+    }
+    else
+    {
+        if (IS_VALID_MOVE_TARGET(ctx, ctx->defence_client))
+        {
+            return TRUE;
+        }
     }
     return FALSE;
 }
