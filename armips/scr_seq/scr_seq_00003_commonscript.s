6a56f1a142d4b1fd38f105271c9a11f8688d43a8
Can O Crisco <zhemker0@gmail.com>
2025-08-04 09:33:17 -0400

    remaking base

--- armips/scr_seq/scr_seq_00003_commonscript.s
+++ armips/scr_seq/scr_seq_00003_commonscript.s
@@ -764,9 +764,11 @@ scr_seq_0003_010:
     goto _0A2E
 
 _0A18:
+    goto_if_set 0x18F, _skipPCOnOff
     scrcmd_500 90
     scrcmd_501 90
     scrcmd_308 90
+_skipPCOnOff:
     return
 
 _0A23:
@@ -1000,7 +1002,10 @@ _0DE7:
 _0DF0:
     closemsg
     play_se SEQ_SE_DP_PC_LOGOFF
+    goto_if_set 0x18F, _skipPCOff
     call _0A23
+_skipPCOff:
+    clearflag 0x18F
     touchscreen_menu_show
     releaseall
     end
@@ -1014,7 +1019,9 @@ _0E02:
 _0E16:
     fade_screen 6, 1, 0, RGB_BLACK
     wait_fade
+    goto_if_set 0x18F, _skipPCTransition
     scrcmd_309 90
+_skipPCTransition:
     return
 
 scr_seq_0003_014:
--- src/field/script_commands.c
+++ src/field/script_commands.c
@@ -15,6 +15,7 @@
 #include "../../include/constants/moves.h"
 #include "../../include/constants/species.h"
 #include "../../include/constants/weather_numbers.h"
+#include "../../include/map_events_internal.h"
 
 /**
  *  @brief script command to give an egg adapted to set the hidden ability
@@ -311,4 +312,58 @@ BOOL ScrCmd_DaycareSanitizeMon(SCRIPTCONTEXT *ctx) {
         }
     }
     return FALSE;
+}
+
+
+/**
+ *  @brief clear overworld request flags
+ *
+ *  @param req OVERWORLD_REQUEST_FLAGS structure to clear
+ */
+void ClearOverworldRequestFlags(OVERWORLD_REQUEST_FLAGS *req)
+{
+    req->TalkCheck    = 0;
+    req->StepCheck    = 0;
+    req->MenuOpen     = 0;
+    req->unk0_0018    = 0;
+    req->CnvButton    = 0;
+    req->MatCheck     = 0;
+    req->PushCheck    = 0;
+    req->MoveCheck    = 0;
+    req->FloatCheck   = 0;
+    req->DebugMenu    = 0;
+    req->DebugBattle  = 0;
+    req->DebugHook    = 0;
+    req->DebugKeyPush = 0;
+
+    req->OpenPCCheck  = 0; // new:  check if pc should be opened
+
+    req->Site = 0xFF;
+    req->PushSite = 0xFF;
+}
+
+/**
+ *  @brief set new overworld request flags depending on buttons pressed
+ *
+ *  @param req OVERWORLD_REQUEST_FLAGS structure to set flags in
+ *  @param trg buttons that are pressed on this frame
+ */
+void SetOverworldRequestFlags(OVERWORLD_REQUEST_FLAGS *req, u16 trg)
+{
+    if (trg & PAD_BUTTON_L) {
+        req->OpenPCCheck = TRUE;
+    }
+}
+
+/**
+ *  @brief handle overworld request flags
+ *
+ *  @param req OVERWORLD_REQUEST_FLAGS structure to set flags in
+ */
+void CheckOverworldRequestFlags(OVERWORLD_REQUEST_FLAGS *req, FieldSystem *fsys)
+{
+    if (req->OpenPCCheck) {
+        SetScriptFlag(0x18F); // some random flag that should be set by script 2010 (file 3 script 10)
+        EventSet_Script(fsys, 2010, NULL); // set up script 2010
+    }
 }
