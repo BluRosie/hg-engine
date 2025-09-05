d7937f3c8d549a0af28c802066c8ce2968f6b9d3
Aero <47219799+Aeliko@users.noreply.github.com>
2025-09-05 20:42:39 +0800

    Fix wild battle gimmick interaction

--- src/individual/ServerBeforeAct.c
+++ src/individual/ServerBeforeAct.c
@@ -126,7 +126,7 @@ void __attribute__((section (".init"))) ServerBeforeActInternal(struct BattleSys
                             }
                             // ai requests mega
                             else {
-                                if (CheckCanMega(sp, client_no)) {
+                                if (CheckCanMega(sp, client_no) && (BattleTypeGet(bw) & (BATTLE_TYPE_TRAINER | BATTLE_TYPE_BATTLE_TOWER))) {
                                     sp->battlemon[client_no].canMega = 1;
                                     newBS.SideMega[client_no] = TRUE;
                                     flag = TRUE;
@@ -144,7 +144,7 @@ void __attribute__((section (".init"))) ServerBeforeActInternal(struct BattleSys
                             }
                             // ai requests mega
                             else {
-                                if (CheckCanMega(sp, client_no)) {
+                                if (CheckCanMega(sp, client_no) && (BattleTypeGet(bw) & (BATTLE_TYPE_TRAINER | BATTLE_TYPE_BATTLE_TOWER))) {
                                     sp->battlemon[client_no].canMega = 1;
                                     newBS.SideMega[1] = TRUE;
                                     newBS.SideMega[3] = TRUE;
