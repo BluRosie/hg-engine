b58c10f6260ceae2ad058b423d46166b597983dc
kumakuma1 <kuehnmaxi@gmail.com>
2025-09-18 08:44:33 +0200

    move cheek pouch to separate script cheek pouch check when activating damage reduction berry

---
+++ data/battle_scripts/subscripts/subscript_0459_PLAY_EAT_BERRY_ANIMATION_CHEEK_POUCH.s
@@ -0,0 +1,12 @@
+.include "asm/include/battle_commands.inc"
+
+.data
+
+_000:
+    PrintAttackMessage
+    Wait 
+    WaitButtonABTime 30
+    PlayBattleAnimation BATTLER_CATEGORY_MSG_TEMP, BATTLE_ANIMATION_EAT_BERRY
+    Wait
+    Call BATTLE_SUBSCRIPT_CHEEK_POUCH
+    End
