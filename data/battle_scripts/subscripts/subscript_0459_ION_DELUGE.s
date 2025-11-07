ecfed9838e5f11821ec6a5bb492056ccc8950c88
Pixelstyx <jsparxs@gmail.com>
2025-11-07 18:36:20 -0500

    update script no for ion deluge

--- data/battle_scripts/subscripts/subscript_0459_ION_DELUGE.s
+++ data/battle_scripts/subscripts/subscript_0459_ION_DELUGE.s
@@ -4,7 +4,7 @@
 
 _Start:
     // A deluge of ions showers the battlefield!
-    PrintMessage 1592, TAG_NONE
+    PrintMessage 1594, TAG_NONE
     Wait
     WaitButtonABTime 30
     UpdateVar OPCODE_FLAG_ON, BSCRIPT_VAR_FIELD_CONDITION, FIELD_CONDITION_ION_DELUGE
