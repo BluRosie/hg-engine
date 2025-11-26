74b2efbd0b7f5de884ece44f8aa356039ecdbd7e
kumakuma1 <kuehnmaxi@gmail.com>
2025-11-26 07:34:21 +0100

    add rock smash item table repoints

---
+++ src/field/rock_smash_item.c
@@ -0,0 +1,37 @@
+#include "../../include/constants/item.h"
+#include "../../include/debug.h"
+#include "../../include/types.h"
+
+
+const u16 sRockSmashItems_Default[] = {
+    ITEM_MAX_ETHER,
+    ITEM_REVIVE,
+    ITEM_HEART_SCALE,
+    ITEM_RED_SHARD,
+    ITEM_BLUE_SHARD,
+    ITEM_GREEN_SHARD,
+    ITEM_YELLOW_SHARD,
+    ITEM_STAR_PIECE,
+};
+
+const u16 sRockSmashItems_RuinsOfAlph[] = {
+    ITEM_RED_SHARD,
+    ITEM_YELLOW_SHARD,
+    ITEM_HELIX_FOSSIL,
+    ITEM_MAX_ETHER,
+    ITEM_BLUE_SHARD,
+    ITEM_GREEN_SHARD,
+    ITEM_OLD_AMBER,
+    ITEM_MAX_REVIVE,
+};
+
+const u16 sRockSmashItems_CliffCave[] = {
+    ITEM_MAX_ETHER,
+    ITEM_PEARL,
+    ITEM_BIG_PEARL,
+    ITEM_RED_SHARD,
+    ITEM_YELLOW_SHARD,
+    ITEM_CLAW_FOSSIL,
+    ITEM_CLAW_FOSSIL,
+    ITEM_RARE_BONE,
+};
