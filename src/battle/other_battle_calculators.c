6dedbeecba7ca017f7ff48f72850ec91e7db758a
Pixelstyx <jsparxs@gmail.com>
2025-12-19 12:33:06 -0500

    Fix type mismatch warning in CheckMoveHealBlocked

--- src/battle/other_battle_calculators.c
+++ src/battle/other_battle_calculators.c
@@ -3004,8 +3004,8 @@ const u16 HealBlockUnusableMoves[] = {
 //  MOVE_POLLEN_PUFF, should be here but can also target enemies when heal blocked so
 };
 
-BOOL LONG_CALL BattleContext_CheckMoveHealBlocked(struct BattleSystem* bsys, struct BattleStruct* ctx, int battlerId, int moveNo) {
-    int i;
+BOOL LONG_CALL BattleContext_CheckMoveHealBlocked(struct BattleSystem* bsys UNUSED, struct BattleStruct* ctx, int battlerId, int moveNo) {
+    u32 i;
     BOOL ret = FALSE;
 
     if (ctx->battlemon[battlerId].moveeffect.healBlockTurns)
