527ab7d47ff0a07033c78f57a6a8263e299d3319
Aero <47219799+Aeliko@users.noreply.github.com>
2025-12-20 17:25:13 +0800

    Make tests run headless

--- scripts/run_tests.py
+++ scripts/run_tests.py
@@ -1,28 +1,41 @@
 from desmume.emulator import DeSmuME
 from desmume.emulator import DeSmuME_Memory
 
+# Settings
 SHOW_VIDEO_OUTPUT = False
 g_EmulatorCommunicationSendHoleAddress = 0x023df15c
 NUMBER_OF_TESTS_TO_RUN = 1
+hasFinishedTesting = False
 
 emu = DeSmuME()
 emu_memory = emu.memory
-emu.open('test.nds')
-emu.backup.import_file('test.sav')
-
-# Create the window for the emulator
-window = emu.create_sdl_window()
 memory = DeSmuME_Memory(emu)
 
-# Run the emulation as fast as possible until quit
-while True:
-    window.process_input()   # Controls are the default DeSmuME controls, see below.
-    emu.cycle()
-    if (SHOW_VIDEO_OUTPUT):
-        window.draw()
-    # TODO: Obviously make it not 1
-    # Kill the emulator after running 1 test case
-    if (emu_memory.unsigned[g_EmulatorCommunicationSendHoleAddress] == NUMBER_OF_TESTS_TO_RUN):
-        break
+def read_communication_hole_value():
+    return emu_memory.signed[g_EmulatorCommunicationSendHoleAddress]
 
-print("Tests complete!\n", flush=True)
+# TODO: Obviously make it not 1
+# Kill the emulator after running 1 test case
+def has_finished_testing():
+    return read_communication_hole_value() == NUMBER_OF_TESTS_TO_RUN
+
+
+def callback_function_when_game_put_thing_into_communication_hole(address, size):
+    if (has_finished_testing()):
+        global hasFinishedTesting
+        hasFinishedTesting = True
+
+memory.register_write(g_EmulatorCommunicationSendHoleAddress, callback_function_when_game_put_thing_into_communication_hole)
+
+def main():
+    emu.open('test.nds')
+    emu.backup.import_file('test.sav')
+
+    # Run the emulation as fast as possible until testing complete
+    while (not hasFinishedTesting):
+        emu.cycle(False)
+
+    print("Tests complete!\n", flush=True)
+
+if __name__ == '__main__':
+    main()
