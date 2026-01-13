762c1a2886a142ad17212fcaf43663a81995be2f
Aero <47219799+Aeliko@users.noreply.github.com>
2026-01-13 21:02:00 +0800

    Make tests work on my Arch laptop

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -1,6 +1,18 @@
+#!/bin/bash
+
+video=false
+
 while getopts 'v' flag; do
     case "${flag}" in
-        v) python3 -u scripts/run_tests.py -v | tee test_logs.txt ;;
-        *) python3 -u scripts/run_tests.py | tee test_logs.txt ;;
+        v) video=true ;;
+        *) ;;
     esac
 done
+
+shift $((OPTIND-1))
+
+if [ "$video" = true ]; then
+    python3 -u scripts/run_tests.py -v | tee test_logs.txt
+else
+    python3 -u scripts/run_tests.py | tee test_logs.txt
+fi
