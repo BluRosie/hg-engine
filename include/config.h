2a48db42386ba3fc8ef967b20fa82d990c4fd4dd
Pixelstyx <jsparxs@gmail.com>
2026-01-19 14:04:29 -0500

    Add config for updated item healing values

--- include/config.h
+++ include/config.h
@@ -190,6 +190,9 @@
 // NATURAL_GIFT_POWER_GEN defines the power of Natural Gift based on generation. Gen 6 or higher are modernized values.
 #define NATURAL_GIFT_POWER_GEN GEN_LATEST
 
+// MEDICINE_HEALING_GEN defines the amount healed by most medicine items. Gen 7 or higher are modernized values.
+#define MEDICINE_HEALING_GEN GEN_LATEST
+
 // BLOCK_LEARNING_UNIMPLEMENTED_MOVES prevents learning moves that are not implemented
 // based on the move having FLAG_UNUSABLE_UNIMPLEMENTED
 #define BLOCK_LEARNING_UNIMPLEMENTED_MOVES
