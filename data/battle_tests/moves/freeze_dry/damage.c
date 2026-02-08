27226100bf597050f9d897e89b0ad727853a0e21
kumakuma1 <kuehnmaxi@gmail.com>
2026-02-08 15:06:43 +0100

    add tests

---
+++ data/battle_tests/moves/freeze_dry/damage.c
@@ -0,0 +1,104 @@
+// Test: Freeze Dry - Super Effective against Water
+#ifndef GET_TEST_CASE_ONLY
+
+#include "../../../../include/battle.h"
+#include "../../../../include/constants/ability.h"
+#include "../../../../include/constants/battle_message_constants.h"
+#include "../../../../include/constants/item.h"
+#include "../../../../include/constants/moves.h"
+#include "../../../../include/constants/species.h"
+#include "../../../../include/test_battle.h"
+
+// each test file is a separate .c file in battle_tests/ for better organization
+const struct TestBattleScenario BattleTests[] = {
+
+#endif
+
+    {
+        .battleType = BATTLE_TYPE_SINGLE,
+        .weather = WEATHER_NONE,
+        .fieldCondition = 0,
+        .terrain = TERRAIN_NONE,
+        .playerParty = {
+            {
+                .species = SPECIES_CRYOGONAL,
+                .level = 50,
+                .form = 0,
+                .ability = ABILITY_LEVITATE,
+                .item = ITEM_NONE,
+                .moves = { MOVE_FREEZE_DRY, MOVE_NONE, MOVE_NONE, MOVE_NONE },
+                .hp = FULL_HP,
+                .status = 0,
+                .condition2 = 0,
+                .moveEffectFlags = 0,
+            },
+            { .species = SPECIES_NONE },
+            { .species = SPECIES_NONE },
+            { .species = SPECIES_NONE },
+            { .species = SPECIES_NONE },
+            { .species = SPECIES_NONE } },
+        .enemyParty = { {
+                            .species = SPECIES_FERALIGATR,
+                            .level = 50,
+                            .form = 0,
+                            .ability = ABILITY_TORRENT,
+                            .item = ITEM_NONE,
+                            .moves = { MOVE_SPLASH, MOVE_NONE, MOVE_NONE, MOVE_NONE },
+                            .hp = FULL_HP,
+                            .status = 0,
+                            .condition2 = 0,
+                            .moveEffectFlags = 0,
+                        },
+            { .species = SPECIES_NONE },
+            { .species = SPECIES_NONE },
+            { .species = SPECIES_NONE },
+            { .species = SPECIES_NONE },
+            { .species = SPECIES_NONE } },
+        .playerScript = { {
+                              { ACTION_MOVE_SLOT_1, BATTLER_ENEMY_FIRST },
+                              { ACTION_NONE, 0 },
+                              { ACTION_NONE, 0 },
+                              { ACTION_NONE, 0 },
+                              { ACTION_NONE, 0 },
+                              { ACTION_NONE, 0 },
+                              { ACTION_NONE, 0 },
+                              { ACTION_NONE, 0 },
+                          },
+            {
+                { ACTION_NONE, 0 },
+                { ACTION_NONE, 0 },
+                { ACTION_NONE, 0 },
+                { ACTION_NONE, 0 },
+                { ACTION_NONE, 0 },
+                { ACTION_NONE, 0 },
+                { ACTION_NONE, 0 },
+                { ACTION_NONE, 0 },
+            } },
+        .enemyScript = { {
+                             { ACTION_MOVE_SLOT_1, BATTLER_PLAYER_FIRST },
+                             { ACTION_NONE, 0 },
+                             { ACTION_NONE, 0 },
+                             { ACTION_NONE, 0 },
+                             { ACTION_NONE, 0 },
+                             { ACTION_NONE, 0 },
+                             { ACTION_NONE, 0 },
+                             { ACTION_NONE, 0 },
+                         },
+            {
+                { ACTION_NONE, 0 },
+                { ACTION_NONE, 0 },
+                { ACTION_NONE, 0 },
+                { ACTION_NONE, 0 },
+                { ACTION_NONE, 0 },
+                { ACTION_NONE, 0 },
+                { ACTION_NONE, 0 },
+                { ACTION_NONE, 0 },
+            } },
+        .expectations = {
+            { .expectationType = EXPECTATION_TYPE_HP_BAR, .battlerIDOrPartySlot = BATTLER_ENEMY_FIRST, .expectationValue.hpTaken = { 90, 90, 92, 92, 96, 96, 96, 98, 98, 98, 102, 102, 102, 104, 104, 108 } },
+            { .expectationType = EXPECTATION_TYPE_MESSAGE, .expectationValue.messageID = BATTLE_MSG_SUPER_EFFECTIVE },
+        }
+    },
+#ifndef GET_TEST_CASE_ONLY
+};
+#endif
