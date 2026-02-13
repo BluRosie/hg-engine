4951526b52eb29fc64ffe9164b7a3d4b30f37ad9
kumakuma1 <kuehnmaxi@gmail.com>
2026-02-13 15:06:27 +0100

    use swoam variable

--- src/individual/ServerDoPostMoveEffects.c
+++ src/individual/ServerDoPostMoveEffects.c
@@ -44,11 +44,11 @@ int ActivateDefenderItems4(void *bsys, struct BattleStruct *sp);
  */
 void __attribute__((section(".init"))) ServerDoPostMoveEffectsInternal(void *bsys, struct BattleStruct *ctx)
 {
-    debug_printf("ServerDoPostMoveEffectsInternal\n");
+    debug_printf("ServerDoPostMoveEffectsInternal %d: movestatus %d, status %d\n", ctx->swoam_seq_no, ctx->waza_status_flag, ctx->server_status_flag);
     int seq_no = 0;
     DynamicSortClientExecutionOrder(bsys, ctx, FALSE);
 
-    switch (ctx->swoak_seq_no) {
+    switch (ctx->swoam_seq_no) {
     case MOVE_PERFORMANCE_VANISH_ON_OFF: {
         int ret = 0;
         for (ctx->swoak_work = 0; ctx->swoak_work < BattleWorkClientSetMaxGet(bsys); ctx->swoak_work++) {
@@ -66,33 +66,48 @@ void __attribute__((section(".init"))) ServerDoPostMoveEffectsInternal(void *bsy
                 return;
             }
         }
-        ctx->swoak_seq_no++;
+        ctx->swoam_seq_no++;
         ctx->swoak_work = 0;
         FALLTHROUGH;
     }
     case MOVE_PERFORMANCE_STEP_2_HIT_SUBSTITUTE:
         // TODO
-        ctx->swoak_seq_no++;
+        ctx->swoam_seq_no++;
         FALLTHROUGH;
     case MOVE_PERFORMANCE_STEP_3_EXPLOSION_USER_FAINTS:
         // TODO
-        ctx->swoak_seq_no++;
+        ctx->swoam_seq_no++;
         FALLTHROUGH;
     case MOVE_PERFORMANCE_STEP_4_DEAL_DAMAGE:
         // TODO
-        ctx->swoak_seq_no++;
+        ctx->swoam_seq_no++;
         FALLTHROUGH;
     case MOVE_PERFORMANCE_STEP_5_SE_TYPE_EFFECTIVENESS_MESSAGE:
         // TODO
-        ctx->swoak_seq_no++;
+        ctx->swoam_seq_no++;
+        if (ServerWazaStatusMessage(bsys, ctx) == TRUE) {
+            return;
+        }
         FALLTHROUGH;
-    case MOVE_PERFORMANCE_STEP_6_NOT_SE_TYPE_EFFECTIVENESS_MESSAGE:
+    case MOVE_PERFORMANCE_STEP_6_NOT_SE_TYPE_EFFECTIVENESS_MESSAGE: {
         // TODO
-        ctx->swoak_seq_no++;
+        ctx->swoam_seq_no++;
+
+        int seq_no;
+
+        if ((ST_ServerAddStatusCheck(bsys, ctx, &seq_no) == TRUE) && ((ctx->waza_status_flag & MOVE_STATUS_FLAG_FAILURE_ANY) == 0)) {
+            LoadBattleSubSeqScript(ctx, ARC_BATTLE_SUB_SEQ, seq_no);
+            ctx->next_server_seq_no = ctx->server_seq_no;
+            ctx->server_seq_no = CONTROLLER_COMMAND_RUN_SCRIPT;
+            return;
+        }
+    }
         FALLTHROUGH;
     case MOVE_PERFORMANCE_STEP_7_CRITICAL_HIT_MESSAGE:
-        // TODO
-        ctx->swoak_seq_no++;
+        ctx->swoam_seq_no++;
+        if (ServerCriticalMessage(bsys, ctx) == TRUE) {
+            return;
+        }
         FALLTHROUGH;
     case MOVE_PERFORMANCE_STEP_8_STURDY_FOCUS_SASH: {
         debug_printf("in MOVE_PERFORMANCE_STEP_8_STURDY_FOCUS_SASH\n");
@@ -103,124 +118,124 @@ void __attribute__((section(".init"))) ServerDoPostMoveEffectsInternal(void *bsy
             ctx->server_seq_no = CONTROLLER_COMMAND_RUN_SCRIPT;
             return;
         }
-        ctx->swoak_seq_no++;
+        ctx->swoam_seq_no++;
         FALLTHROUGH;
     }
     case MOVE_PERFORMANCE_STEP_8_1_SURVIVE_WITH_FRIENDSHIP:
         // TODO
-        ctx->swoak_seq_no++;
+        ctx->swoam_seq_no++;
         FALLTHROUGH;
     case MOVE_PERFORMANCE_STEP_9_0_FLING:
         // TODO
-        ctx->swoak_seq_no++;
+        ctx->swoam_seq_no++;
         FALLTHROUGH;
     case MOVE_PERFORMANCE_STEP_9_1_ADDITIONAL_EFFECTS:
         // TODO
-        ctx->swoak_seq_no++;
+        ctx->swoam_seq_no++;
         FALLTHROUGH;
     case MOVE_PERFORMANCE_STEP_9_2_LOWERING_STATS:
         // TODO
-        ctx->swoak_seq_no++;
+        ctx->swoam_seq_no++;
         FALLTHROUGH;
     case MOVE_PERFORMANCE_STEP_9_3_HP_DRAINING_MOVES:
         // TODO
-        ctx->swoak_seq_no++;
+        ctx->swoam_seq_no++;
         FALLTHROUGH;
     case MOVE_PERFORMANCE_STEP_9_4_FLAME_BURST:
         // TODO
-        ctx->swoak_seq_no++;
+        ctx->swoam_seq_no++;
         FALLTHROUGH;
     case MOVE_PERFORMANCE_STEP_9_5_DYNAMAX_MOVE_EFFECTS:
         // TODO
-        ctx->swoak_seq_no++;
+        ctx->swoam_seq_no++;
         FALLTHROUGH;
     case MOVE_PERFORMANCE_STEP_10_0_CORE_ENFORCER:
         // TODO
-        ctx->swoak_seq_no++;
+        ctx->swoam_seq_no++;
         FALLTHROUGH;
     case MOVE_PERFORMANCE_STEP_10_1_RAGE:
         // TODO
-        ctx->swoak_seq_no++;
+        ctx->swoam_seq_no++;
         FALLTHROUGH;
     case MOVE_PERFORMANCE_STEP_10_2_CLEAR_SMOG:
         // TODO
-        ctx->swoak_seq_no++;
+        ctx->swoam_seq_no++;
         FALLTHROUGH;
     case MOVE_PERFORMANCE_STEP_10_3_GRUDGE:
         // TODO
-        ctx->swoak_seq_no++;
+        ctx->swoam_seq_no++;
         FALLTHROUGH;
     case MOVE_PERFORMANCE_STEP_10_4_BEAK_BLAST_BURN:
         // TODO
-        ctx->swoak_seq_no++;
+        ctx->swoam_seq_no++;
         FALLTHROUGH;
     case MOVE_PERFORMANCE_STEP_10_5_POISON_TOUCH:
         // TODO
-        ctx->swoak_seq_no++;
+        ctx->swoam_seq_no++;
         FALLTHROUGH;
     case MOVE_PERFORMANCE_STEP_10_6_DEFENDER_ABILITY:
         // TODO
-        ctx->swoak_seq_no++;
+        ctx->swoam_seq_no++;
         FALLTHROUGH;
     case MOVE_PERFORMANCE_STEP_10_7_COTTON_DOWN:
         // TODO
-        ctx->swoak_seq_no++;
+        ctx->swoam_seq_no++;
         FALLTHROUGH;
     case MOVE_PERFORMANCE_STEP_10_8_DAMAGE_REDUCTION_BERRY:
         // TODO
-        ctx->swoak_seq_no++;
+        ctx->swoam_seq_no++;
         FALLTHROUGH;
     case MOVE_PERFORMANCE_STEP_10_9_DEFENDER_ITEMS_1:
         // TODO
-        ctx->swoak_seq_no++;
+        ctx->swoam_seq_no++;
         FALLTHROUGH;
     case MOVE_PERFORMANCE_STEP_10_10_INCINERATE:
         // TODO
-        ctx->swoak_seq_no++;
+        ctx->swoam_seq_no++;
         FALLTHROUGH;
     case MOVE_PERFORMANCE_STEP_10_11_DEFENDER_ITEMS_2_JABOCA_ROWAP:
         // TODO
-        ctx->swoak_seq_no++;
+        ctx->swoam_seq_no++;
         FALLTHROUGH;
     case MOVE_PERFORMANCE_STEP_10_12_DISGUISE_ICE_FACE:
         // TODO
-        ctx->swoak_seq_no++;
+        ctx->swoam_seq_no++;
         FALLTHROUGH;
     case MOVE_PERFORMANCE_STEP_10_13_PROTECTION_FROM_Z_MOVE:
         // TODO
-        ctx->swoak_seq_no++;
+        ctx->swoam_seq_no++;
         FALLTHROUGH;
     case MOVE_PERFORMANCE_STEP_11_0_FAINTING:
         // TODO
-        ctx->swoak_seq_no++;
+        ctx->swoam_seq_no++;
         FALLTHROUGH;
     case MOVE_PERFORMANCE_STEP_11_1_DESTINY_BOND:
         // TODO
-        ctx->swoak_seq_no++;
+        ctx->swoam_seq_no++;
         FALLTHROUGH;
     case MOVE_PERFORMANCE_STEP_12_0_RESET_UNNERVE_NEUTRALIZING_GAS_IF_FAINTED:
         // TODO
-        ctx->swoak_seq_no++;
+        ctx->swoam_seq_no++;
         FALLTHROUGH;
     case MOVE_PERFORMANCE_STEP_13_0_MULTIHIT_MOVE_ATTACKER_ITEMS_4:
         // TODO
-        ctx->swoak_seq_no++;
+        ctx->swoam_seq_no++;
         FALLTHROUGH;
-    case MOVE_PERFORMANCE_STEP_13_1_MULTIHIT_MOVE_DEFENDER_ITEMS_4:
+    case MOVE_PERFORMANCE_STEP_13_1_MULTIHIT_MOVE_DEFENDER_ITEMS_4: //Go back to step 1, damage calc
         // TODO
-        ctx->swoak_seq_no++;
+        ctx->swoam_seq_no++;
         FALLTHROUGH;
     case MOVE_PERFORMANCE_STEP_14_0_FRIENDSHIP_MESSAGE:
         // TODO
-        ctx->swoak_seq_no++;
+        ctx->swoam_seq_no++;
         FALLTHROUGH;
     case MOVE_PERFORMANCE_STEP_15_0_RECOIL_DAMAGE:
         // TODO
-        ctx->swoak_seq_no++;
+        ctx->swoam_seq_no++;
         FALLTHROUGH;
     case MOVE_PERFORMANCE_STEP_15_1_BIND_STATE:
         // TODO
-        ctx->swoak_seq_no++;
+        ctx->swoam_seq_no++;
         FALLTHROUGH;
     case MOVE_PERFORMANCE_STEP_15_2_SCALE_SHOT_FELL_STINGER:
         // TODO
@@ -241,67 +256,67 @@ void __attribute__((section(".init"))) ServerDoPostMoveEffectsInternal(void *bsy
             }
         }
 
-        ctx->swoak_seq_no++;
+        ctx->swoam_seq_no++;
         FALLTHROUGH;
     case MOVE_PERFORMANCE_STEP_15_3_KNOCK_OFF_THIEF_PLUCK:
         // TODO
-        ctx->swoak_seq_no++;
+        ctx->swoam_seq_no++;
         FALLTHROUGH;
     case MOVE_PERFORMANCE_STEP_15_4_CIRCLE_THROW_DRAGON_TAIL:
         // TODO
-        ctx->swoak_seq_no++;
+        ctx->swoam_seq_no++;
         FALLTHROUGH;
     case MOVE_PERFORMANCE_STEP_15_5_KNOCK_DOWN_THOUSAND_WAVE_JAW_LOCK:
         // TODO
-        ctx->swoak_seq_no++;
+        ctx->swoam_seq_no++;
         FALLTHROUGH;
     case MOVE_PERFORMANCE_STEP_15_5_PLASMA_FISTS:
         // TODO
-        ctx->swoak_seq_no++;
+        ctx->swoam_seq_no++;
         FALLTHROUGH;
     case MOVE_PERFORMANCE_STEP_15_6_GENESIS_SUPERNOVA:
         // TODO
-        ctx->swoak_seq_no++;
+        ctx->swoam_seq_no++;
         FALLTHROUGH;
     case MOVE_PERFORMANCE_STEP_15_7_RAPID_SPIN:
         // TODO
-        ctx->swoak_seq_no++;
+        ctx->swoam_seq_no++;
         FALLTHROUGH;
     case MOVE_PERFORMANCE_STEP_15_8_THAW_FROM_FIRE_MOVE:
         // TODO
-        ctx->swoak_seq_no++;
+        ctx->swoam_seq_no++;
         FALLTHROUGH;
     case MOVE_PERFORMANCE_STEP_15_9_SMELLING_SALTS_WAKEUP_SLAP_SPARKLING_ARIA:
         // TODO
-        ctx->swoak_seq_no++;
+        ctx->swoam_seq_no++;
         FALLTHROUGH;
     case MOVE_PERFORMANCE_STEP_15_10_EERIE_SPELL:
         // TODO
-        ctx->swoak_seq_no++;
+        ctx->swoam_seq_no++;
         FALLTHROUGH;
     case MOVE_PERFORMANCE_STEP_16_0_MAGICIAN_MOXIE:
         // TODO
-        ctx->swoak_seq_no++;
+        ctx->swoam_seq_no++;
         FALLTHROUGH;
     case MOVE_PERFORMANCE_STEP_16_1_BERSERK_COLOR_CHANGE:
         // TODO
-        ctx->swoak_seq_no++;
+        ctx->swoam_seq_no++;
         FALLTHROUGH;
     case MOVE_PERFORMANCE_STEP_17_0_DEFENDER_ITEMS_3:
         // TODO
-        ctx->swoak_seq_no++;
+        ctx->swoam_seq_no++;
         FALLTHROUGH;
     case MOVE_PERFORMANCE_STEP_18_0_PLEDGE_MOVES_COMBINATION:
         // TODO
-        ctx->swoak_seq_no++;
+        ctx->swoam_seq_no++;
         FALLTHROUGH;
     case MOVE_PERFORMANCE_STEP_19_0_FORM_CHANGE:
         // TODO
-        ctx->swoak_seq_no++;
+        ctx->swoam_seq_no++;
         FALLTHROUGH;
     case MOVE_PERFORMANCE_STEP_20_0_LIFE_ORB_SHELL_BELL:
         // TODO
-        ctx->swoak_seq_no++;
+        ctx->swoam_seq_no++;
         FALLTHROUGH;
     case MOVE_PERFORMANCE_STEP_21_0_MOVE_DEFENDER_ITEMS_4:
         debug_printf("in MOVE_PERFORMANCE_STEP_21_0_MOVE_DEFENDER_ITEMS_4\n");
@@ -309,70 +324,70 @@ void __attribute__((section(".init"))) ServerDoPostMoveEffectsInternal(void *bsy
         if (ActivateDefenderItems4(bsys, ctx) == TRUE) {
             return;
         }
-        ctx->swoak_seq_no++;
+        ctx->swoam_seq_no++;
         FALLTHROUGH;
     case MOVE_PERFORMANCE_STEP_22_0_EMERGENCY_EXIT_WIMP_OUT:
         // TODO
-        ctx->swoak_seq_no++;
+        ctx->swoam_seq_no++;
         FALLTHROUGH;
     case MOVE_PERFORMANCE_STEP_23_0_U_TURN_VOLT_SWITCH:
         // TODO
-        ctx->swoak_seq_no++;
+        ctx->swoam_seq_no++;
         FALLTHROUGH;
     case MOVE_PERFORMANCE_STEP_24_0_PICKPOCKET:
         // TODO
-        ctx->swoak_seq_no++;
+        ctx->swoam_seq_no++;
         FALLTHROUGH;
     case MOVE_PERFORMANCE_STEP_25_0_BURN_UP_DOUBLE_SHOCK_TYPELOSS:
         // TODO
-        ctx->swoak_seq_no++;
+        ctx->swoam_seq_no++;
         FALLTHROUGH;
     case MOVE_PERFORMANCE_STEP_25_1_NATURAL_GIFT:
         // TODO
-        ctx->swoak_seq_no++;
+        ctx->swoam_seq_no++;
         FALLTHROUGH;
     case MOVE_PERFORMANCE_STEP_25_2_OUTRAGE_CONFUSION:
         // TODO
-        ctx->swoak_seq_no++;
+        ctx->swoam_seq_no++;
         FALLTHROUGH;
     case MOVE_PERFORMANCE_STEP_25_3_ICE_SPINNER_STEEL_ROLLER:
         // TODO
-        ctx->swoak_seq_no++;
+        ctx->swoam_seq_no++;
         FALLTHROUGH;
     case MOVE_PERFORMANCE_STEP_25_4_ORDER_UP:
         // TODO
-        ctx->swoak_seq_no++;
+        ctx->swoam_seq_no++;
         FALLTHROUGH;
     case MOVE_PERFORMANCE_STEP_26_0_LEPPA_BERRY_THROAT_SPRAY_BLUNDER_POLICY:
         // TODO
-        ctx->swoak_seq_no++;
+        ctx->swoam_seq_no++;
         FALLTHROUGH;
     case MOVE_PERFORMANCE_STEP_27_0_ABILITIES_2:
         // TODO
-        ctx->swoak_seq_no++;
+        ctx->swoam_seq_no++;
         FALLTHROUGH;
     case MOVE_PERFORMANCE_STEP_27_1_OPPORTUNIST_SYBIOSIS:
         // TODO
-        ctx->swoak_seq_no++;
+        ctx->swoam_seq_no++;
         FALLTHROUGH;
     case MOVE_PERFORMANCE_STEP_28_0_WHITE_HERB_MIRROR_HERB_EJECT_PACK:
         // TODO
-        ctx->swoak_seq_no++;
+        ctx->swoam_seq_no++;
         FALLTHROUGH;
     case MOVE_PERFORMANCE_STEP_29_0_SOMETHING_WITH_SWITCHING: // send out new mon?
         // TODO
-        ctx->swoak_seq_no++;
+        ctx->swoam_seq_no++;
         FALLTHROUGH;
     case MOVE_PERFORMANCE_STEP_30_0_DANCER:
         // TODO
-        ctx->swoak_seq_no++;
+        ctx->swoam_seq_no++;
         FALLTHROUGH;
     case MOVE_PERFORMANCE_CLEAR_MAGIC_COAT:
         ctx->magicBounceTracker = FALSE;
-        ctx->swoak_seq_no++;
+        ctx->swoam_seq_no++;
         FALLTHROUGH;
     case MOVE_PERFORMANCE_END:
-        ctx->swoak_seq_no++;
+        ctx->swoam_seq_no++;
         break;
     default:
         break;
@@ -383,7 +398,7 @@ void __attribute__((section(".init"))) ServerDoPostMoveEffectsInternal(void *bsy
     /*
     // Sort clients because moves may affect ctxeed
     DynamicSortClientExecutionOrder(bsys, ctx, FALSE);
-    switch (ctx->swoak_seq_no) {
+    switch (ctx->swoam_seq_no) {
         case SWOAK_SEQ_VANISH_ON_OFF: {
             int ret = 0;
             while (ctx->swoak_work < BattleWorkClientSetMaxGet(bsys))
@@ -405,11 +420,11 @@ void __attribute__((section(".init"))) ServerDoPostMoveEffectsInternal(void *bsy
                     return;
             }
         }
-        ctx->swoak_seq_no++;
+        ctx->swoam_seq_no++;
         ctx->swoak_work = 0;
         FALLTHROUGH;
     case SWOAK_SEQ_SYNCHRONIZE_CHECK:
-        ctx->swoak_seq_no++;
+        ctx->swoam_seq_no++;
         if (SynchroniseAbilityCheck(bsys, ctx, ctx->server_seq_no) == TRUE)
             return;
         FALLTHROUGH;
@@ -427,15 +442,15 @@ void __attribute__((section(".init"))) ServerDoPostMoveEffectsInternal(void *bsy
                 return;
             }
         }
-        ctx->swoak_seq_no++;
+        ctx->swoam_seq_no++;
         FALLTHROUGH;
     case SWOAK_SEQ_CHECK_HELD_ITEM_EFFECT_ATTACKER:
-        ctx->swoak_seq_no++;
+        ctx->swoam_seq_no++;
         if (TryUseHeldItem(bsys, ctx, ctx->attack_client) == TRUE) // will eventually need TryUseHeldItem anyway.  generic berry function thing
             return;
         FALLTHROUGH;
     case SWOAK_SEQ_CHECK_HELD_ITEM_EFFECT_DEFENDER:
-        ctx->swoak_seq_no++;
+        ctx->swoam_seq_no++;
         if (ctx->defence_client != 0xFF)
         {
             if (TryUseHeldItem(bsys, ctx, ctx->defence_client) == TRUE)
@@ -446,7 +461,7 @@ void __attribute__((section(".init"))) ServerDoPostMoveEffectsInternal(void *bsy
         {
             int seq_no;
 
-            ctx->swoak_seq_no++;
+            ctx->swoam_seq_no++;
             if (CheckDefenderItemEffectOnHit(bsys, ctx, &seq_no) == TRUE)
             {
                 LoadBattleSubSeqScript(ctx, ARC_BATTLE_SUB_SEQ, seq_no);
@@ -463,7 +478,7 @@ void __attribute__((section(".init"))) ServerDoPostMoveEffectsInternal(void *bsy
 
             movetype = GetAdjustedMoveType(ctx, ctx->attack_client, currMove); // new normalize checks
 
-            ctx->swoak_seq_no++;
+            ctx->swoam_seq_no++;
 
             if (ctx->defence_client != 0xFF)
             {
@@ -514,19 +529,19 @@ void __attribute__((section(".init"))) ServerDoPostMoveEffectsInternal(void *bsy
 
             if (ret == 0)
             {
-                ctx->swoak_seq_no++;
+                ctx->swoam_seq_no++;
                 ctx->swoak_work = 0;
             }
         }
         FALLTHROUGH;
     case SWOAK_SEQ_CLEAR_MAGIC_COAT:
         ctx->magicBounceTracker = FALSE;
-        ctx->swoak_seq_no++;
+        ctx->swoam_seq_no++;
         break;
     default:
         break;
     }
-    //ctx->swoak_seq_no = 0;
+    //ctx->swoam_seq_no = 0;
     ctx->swoak_work = 0;
     ctx->server_seq_no = CONTROLLER_COMMAND_32;
     
