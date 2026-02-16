47b27196cf98e89197d4733704aaa94a3081419e
kumakuma1 <kuehnmaxi@gmail.com>
2026-02-16 21:10:53 +0100

    uturn test

---
+++ data/battle_tests/moves/u_turn/berry_and_life_orb.c
@@ -0,0 +1,118 @@
+// Test: U-Turn - Iron Barbs, Life Orb, Switch, Sitrus
+#ifndef GET_TEST_CASE_ONLY
+
+#include "../../../../include/battle.h"
+#include "../../../../include/constants/ability.h"
+#include "../../../../include/constants/battle_message_constants.h"
+#include "../../../../include/constants/item.h"
+#include "../../../../include/constants/moves.h"
+#include "../../../../include/constants/species.h"
+#include "../../../../include/test_battle.h"
+
+// each test file is a separate .c file in battle_tests/ for better organization
+const struct TestBattleScenario BattleTests[] = {
+
+#endif
+
+    {
+        .battleType = BATTLE_TYPE_SINGLE,
+        .weather = WEATHER_NONE,
+        .fieldCondition = 0,
+        .terrain = TERRAIN_NONE,
+        .playerParty = {
+            {
+                .species = SPECIES_BEEDRILL,
+                .level = 50,
+                .form = 0,
+                .ability = ABILITY_SWARM,
+                .item = ITEM_LIFE_ORB,
+                .moves = { MOVE_U_TURN, MOVE_NONE, MOVE_NONE, MOVE_NONE },
+                .hp = FULL_HP,
+                .status = 0,
+                .condition2 = 0,
+                .moveEffectFlags = 0,
+            },
+            {
+                .species = SPECIES_BEEDRILL,
+                .level = 49,
+                .form = 0,
+                .ability = ABILITY_SWARM,
+                .item = ITEM_LIFE_ORB,
+                .moves = { MOVE_U_TURN, MOVE_NONE, MOVE_NONE, MOVE_NONE },
+                .hp = FULL_HP,
+                .status = 0,
+                .condition2 = 0,
+                .moveEffectFlags = 0,
+            },
+            { .species = SPECIES_NONE },
+            { .species = SPECIES_NONE },
+            { .species = SPECIES_NONE },
+            { .species = SPECIES_NONE } },
+        .enemyParty = { {
+                            .species = SPECIES_FERROSEED,
+                            .level = 45,
+                            .form = 0,
+                            .ability = ABILITY_IRON_BARBS,
+                            .item = ITEM_SITRUS_BERRY,
+                            .moves = { MOVE_SPLASH, MOVE_NONE, MOVE_NONE, MOVE_NONE },
+                            .hp = FULL_HP,
+                            .status = 0,
+                            .condition2 = 0,
+                            .moveEffectFlags = 0,
+                        },
+            { .species = SPECIES_NONE },
+            { .species = SPECIES_NONE },
+            { .species = SPECIES_NONE },
+            { .species = SPECIES_NONE },
+            { .species = SPECIES_NONE } },
+        .playerScript = { {
+                              { ACTION_MOVE_SLOT_1, BATTLER_ENEMY_FIRST },
+                              { ACTION_NONE, 0 },
+                              { ACTION_NONE, 0 },
+                              { ACTION_NONE, 0 },
+                              { ACTION_NONE, 0 },
+                              { ACTION_NONE, 0 },
+                              { ACTION_NONE, 0 },
+                              { ACTION_NONE, 0 },
+                          },
+            {
+                { ACTION_NONE, 0 },
+                { ACTION_NONE, 0 },
+                { ACTION_NONE, 0 },
+                { ACTION_NONE, 0 },
+                { ACTION_NONE, 0 },
+                { ACTION_NONE, 0 },
+                { ACTION_NONE, 0 },
+                { ACTION_NONE, 0 },
+            } },
+        .enemyScript = { {
+                             { ACTION_MOVE_SLOT_1, BATTLER_PLAYER_FIRST },
+                             { ACTION_NONE, 0 },
+                             { ACTION_NONE, 0 },
+                             { ACTION_NONE, 0 },
+                             { ACTION_NONE, 0 },
+                             { ACTION_NONE, 0 },
+                             { ACTION_NONE, 0 },
+                             { ACTION_NONE, 0 },
+                         },
+            {
+                { ACTION_NONE, 0 },
+                { ACTION_NONE, 0 },
+                { ACTION_NONE, 0 },
+                { ACTION_NONE, 0 },
+                { ACTION_NONE, 0 },
+                { ACTION_NONE, 0 },
+                { ACTION_NONE, 0 },
+                { ACTION_NONE, 0 },
+            } },
+        .expectations = {
+            { .expectationType = EXPECTATION_TYPE_HP_BAR, .battlerIDOrPartySlot = BATTLER_ENEMY_FIRST, .expectationValue.hpTaken = { 56, 58, 58, 58, 60, 60, 60, 62, 62, 62, 64, 64, 64, 66, 66, 68 } },
+            { .expectationType = EXPECTATION_TYPE_MESSAGE, .expectationValue.messageID = BATTLE_MSG_ABILITY_HURT_ON_HIT },
+            { .expectationType = EXPECTATION_TYPE_MESSAGE, .expectationValue.messageID = BATTLE_MSG_LIFE_ORB_DAMAGE },
+            { .expectationType = EXPECTATION_TYPE_MESSAGE, .expectationValue.messageID = BATTLE_MSG_HELD_ITEM_HP_RESTORE },
+            { .expectationType = EXPECTATION_TYPE_MESSAGE, .expectationValue.messageID = BATTLE_MSG_WENT_BACK_TO },
+        } 
+    },
+#ifndef GET_TEST_CASE_ONLY
+};
+#endif
