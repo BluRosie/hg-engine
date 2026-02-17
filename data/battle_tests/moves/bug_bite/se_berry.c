e74e1634d6770087726534ae07d6f75ad068cf1f
kumakuma1 <kuehnmaxi@gmail.com>
2026-02-17 16:56:28 +0100

    bugbite test

---
+++ data/battle_tests/moves/bug_bite/se_berry.c
@@ -0,0 +1,103 @@
+// Test: Bug Bite - Do not eat SE berry
+#ifndef GET_TEST_CASE_ONLY
+
+#include "../../../../include/battle.h"
+#include "../../../../include/constants/ability.h"
+#include "../../../../include/constants/battle_message_constants.h"
+#include "../../../../include/constants/item.h"
+#include "../../../../include/constants/moves.h"
+#include "../../../../include/constants/species.h"
+#include "../../../../include/test_battle.h"
+
+// each test file is a separate .c file in battle_tests/ for better organization
+const struct TestBattleScenario BattleTests[] = {
+
+#endif
+
+    {   .battleType = BATTLE_TYPE_SINGLE,
+        .weather = WEATHER_NONE,
+        .fieldCondition = 0,
+        .terrain = TERRAIN_NONE,
+        .playerParty = {
+            {
+                .species = SPECIES_BEEDRILL,
+                .level = 50,
+                .form = 0,
+                .ability = ABILITY_SWARM,
+                .item = ITEM_NONE,
+                .moves = { MOVE_BUG_BITE, MOVE_NONE, MOVE_NONE, MOVE_NONE },
+                .hp = FULL_HP,
+                .status = 0,
+                .condition2 = 0,
+                .moveEffectFlags = 0,
+            },
+            { .species = SPECIES_NONE },
+            { .species = SPECIES_NONE },
+            { .species = SPECIES_NONE },
+            { .species = SPECIES_NONE },
+            { .species = SPECIES_NONE } },
+        .enemyParty = { {
+                            .species = SPECIES_MEGANIUM,
+                            .level = 50,
+                            .form = 0,
+                            .ability = ABILITY_CHEEK_POUCH,
+                            .item = ITEM_TANGA_BERRY,
+                            .moves = { MOVE_SPLASH, MOVE_NONE, MOVE_NONE, MOVE_NONE },
+                            .hp = 60,
+                            .status = 0,
+                            .condition2 = 0,
+                            .moveEffectFlags = 0,
+                        },
+            { .species = SPECIES_NONE },
+            { .species = SPECIES_NONE },
+            { .species = SPECIES_NONE },
+            { .species = SPECIES_NONE },
+            { .species = SPECIES_NONE } },
+        .playerScript = { {
+                              { ACTION_MOVE_SLOT_1, BATTLER_ENEMY_FIRST },
+                              { ACTION_NONE, 0 },
+                              { ACTION_NONE, 0 },
+                              { ACTION_NONE, 0 },
+                              { ACTION_NONE, 0 },
+                              { ACTION_NONE, 0 },
+                              { ACTION_NONE, 0 },
+                              { ACTION_NONE, 0 },
+                          },
+            {
+                { ACTION_NONE, 0 },
+                { ACTION_NONE, 0 },
+                { ACTION_NONE, 0 },
+                { ACTION_NONE, 0 },
+                { ACTION_NONE, 0 },
+                { ACTION_NONE, 0 },
+                { ACTION_NONE, 0 },
+                { ACTION_NONE, 0 },
+            } },
+        .enemyScript = { {
+                             { ACTION_MOVE_SLOT_1, BATTLER_PLAYER_FIRST },
+                             { ACTION_NONE, 0 },
+                             { ACTION_NONE, 0 },
+                             { ACTION_NONE, 0 },
+                             { ACTION_NONE, 0 },
+                             { ACTION_NONE, 0 },
+                             { ACTION_NONE, 0 },
+                             { ACTION_NONE, 0 },
+                         },
+            {
+                { ACTION_NONE, 0 },
+                { ACTION_NONE, 0 },
+                { ACTION_NONE, 0 },
+                { ACTION_NONE, 0 },
+                { ACTION_NONE, 0 },
+                { ACTION_NONE, 0 },
+                { ACTION_NONE, 0 },
+                { ACTION_NONE, 0 },
+            } },
+        .expectations = {
+            { .expectationType = EXPECTATION_TYPE_MESSAGE, .expectationValue.messageID = BATTLE_MSG_BERRY_WEAKENED_SE_MOVE }, 
+            { .expectationType = EXPECTATION_TYPE_MESSAGE, .expectationValue.messageID = BATTLE_MSG_CHEEK_POUCH }, 
+        },
+    },
+#ifndef GET_TEST_CASE_ONLY
+};
+#endif
