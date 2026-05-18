2bd27c251cc9dc60b8fbe98cbd2da1466c458b65
kumakuma1 <kuehnmaxi@mail.com>
2026-05-18 08:15:22 +0200

    Revert "fix howl"
    This reverts commit f729b3004a9300901bdc0b20bbccdce9ec0cd1f3.

--- data/battle_scripts/subscripts/subscript_0465_HOWL.s
+++ data/battle_scripts/subscripts/subscript_0465_HOWL.s
@@ -3,4 +3,8 @@
 .data
 
 _Start:
+    PrintAttackMessage
+    PlayMoveAnimation BATTLER_CATEGORY_ATTACKER
+    Wait 
+    UpdateVarFromVar OPCODE_SET, BSCRIPT_VAR_MSG_BATTLER_TEMP, BSCRIPT_VAR_BATTLER_DEFENDER
     Call BATTLE_SUBSCRIPT_UPDATE_STAT_STAGE
