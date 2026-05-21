38f1c038b27eec6b747c68a98cdad09315afbf98
kumakuma1 <kuehnmaxi@gmail.com>
2026-05-21 13:01:45 +0200

    pursuit test

---
+++ data/battle_tests/moves/pursuit/pursuit_without_switch.c
@@ -0,0 +1,112 @@
+// Test: Pursuit - normal attack without switching
+#ifndef GET_TEST_CASE_ONLY
+
+#include "../../../../include/battle.h"
+#include "../../../../include/constants/ability.h"
+#include "../../../../include/constants/battle_message_constants.h"
+#include "../../../../include/constants/item.h"
+#include "../../../../include/constants/moves.h"
+#include "../../../../include/constants/species.h"
+#include "../../../../include/test_battle.h"
+
+// each test file is a separate .c file in battle_tests/ for better organization
+const struct TestBattleScenario BattleTests[] = {
+
+#endif
+
+    {   .battleType = BATTLE_TYPE_SINGLE,
+        .weather = WEATHER_NONE,
+        .fieldCondition = 0,
+        .terrain = TERRAIN_NONE,
+        .playerParty = {
+            {
+                .species = SPECIES_UMBREON,
+                .level = 50,
+                .form = 0,
+                .ability = ABILITY_SYNCHRONIZE,
+                .item = ITEM_LIFE_ORB,
+                .moves = { MOVE_PURSUIT, MOVE_NONE, MOVE_NONE, MOVE_NONE },
+                .hp = FULL_HP,
+                .status = 0,
+                .condition2 = 0,
+                .moveEffectFlags = 0,
+            },
+            { .species = SPECIES_NONE },
+            { .species = SPECIES_NONE },
+            { .species = SPECIES_NONE },
+            { .species = SPECIES_NONE },
+            { .species = SPECIES_NONE }
+        },
+        .enemyParty = { 
+                        {
+                            .species = SPECIES_GARCHOMP,
+                            .level = 50,
+                            .form = 0,
+                            .ability = ABILITY_ROUGH_SKIN,
+                            .item = ITEM_NONE,
+                            .moves = { MOVE_SLEEP_TALK, MOVE_NONE, MOVE_NONE, MOVE_NONE },
+                            .hp = FULL_HP,
+                            .status = 0,
+                            .condition2 = 0,
+                            .moveEffectFlags = 0,
+                        },
+            { .species = SPECIES_NONE },
+            { .species = SPECIES_NONE },
+            { .species = SPECIES_NONE },
+            { .species = SPECIES_NONE },
+            { .species = SPECIES_NONE }
+        },
+        .playerScript = {
+            {
+                              { ACTION_MOVE_SLOT_1, BATTLER_ENEMY_FIRST },
+                              { ACTION_NONE, 0 },
+                              { ACTION_NONE, 0 },
+                              { ACTION_NONE, 0 },
+                              { ACTION_NONE, 0 },
+                              { ACTION_NONE, 0 },
+                              { ACTION_NONE, 0 },
+                              { ACTION_NONE, 0 },
+                          },
+            {
+                { ACTION_NONE, 0 },
+                { ACTION_NONE, 0 },
+                { ACTION_NONE, 0 },
+                { ACTION_NONE, 0 },
+                { ACTION_NONE, 0 },
+                { ACTION_NONE, 0 },
+                { ACTION_NONE, 0 },
+                { ACTION_NONE, 0 },
+            }
+        },
+        .enemyScript = {
+            {
+                             { ACTION_MOVE_SLOT_1, BATTLER_PLAYER_FIRST },
+                             { ACTION_NONE, 0 },
+                             { ACTION_NONE, 0 },
+                             { ACTION_NONE, 0 },
+                             { ACTION_NONE, 0 },
+                             { ACTION_NONE, 0 },
+                             { ACTION_NONE, 0 },
+                             { ACTION_NONE, 0 },
+                         },
+            {
+                { ACTION_NONE, 0 },
+                { ACTION_NONE, 0 },
+                { ACTION_NONE, 0 },
+                { ACTION_NONE, 0 },
+                { ACTION_NONE, 0 },
+                { ACTION_NONE, 0 },
+                { ACTION_NONE, 0 },
+                { ACTION_NONE, 0 },
+            }
+        },
+        .expectations = {
+            { .expectationType = EXPECTATION_TYPE_MESSAGE, .expectationValue.message = "Umbreon used Pursuit!" },
+            { .expectationType = EXPECTATION_TYPE_HP_BAR, .battlerIDOrPartySlot = BATTLER_ENEMY_FIRST, .expectationValue.hpTaken = { 23, 23, 25, 25, 25, 25, 25, 25, 25, 27, 27, 27, 27, 27, 27, 29 } },
+            { .expectationType = EXPECTATION_TYPE_MESSAGE, .expectationValue.message = "The opposing Garchomp's Rough Skin hurt Umbreon!" },
+            { .expectationType = EXPECTATION_TYPE_MESSAGE, .expectationValue.message = "Umbreon lost some of its HP!" },
+        },
+    },
+#ifndef GET_TEST_CASE_ONLY
+};
+#endif
