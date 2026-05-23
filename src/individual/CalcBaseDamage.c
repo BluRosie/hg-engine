380dcee4873bccc5553f621c23cf296cbf1c5fde
kumakuma1 <kuehnmaxi@gmail.com>
2026-05-23 16:17:05 +0200

    remove debug output

--- src/individual/CalcBaseDamage.c
+++ src/individual/CalcBaseDamage.c
@@ -403,12 +403,6 @@ int UNUSED CalcBaseDamageInternal(struct BattleSystem *bw, struct BattleStruct *
 
     switch (moveno) {
     case MOVE_PURSUIT:
-        debug_printf("Pursuit atk %d, def %d, switchId %d\n", attacker, defender, sp->reshuffle_client);
-        debug_printf("ctx->playerActions[%d][0] = %d\n", defender, sp->playerActions[defender][0]);
-        debug_printf("ctx->playerActions[%d][1] = %d\n", defender, sp->playerActions[defender][1]);
-        debug_printf("ctx->playerActions[%d][2] = %d\n", defender, sp->playerActions[defender][2]);
-        debug_printf("ctx->playerActions[%d][3] = %d\n", defender, sp->playerActions[defender][3]);
-        //if (defender == sp->reshuffle_client && (sp->playerActions[defender][3] == CONTROLLER_COMMAND_40)) {
         if (sp->pursuitContext.isActive) {
             basePowerModifier = QMul_RoundUp(basePowerModifier, UQ412__2_0);
         }
