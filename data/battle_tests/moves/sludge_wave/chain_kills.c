c7336970f1f6d2a534b23acc01b2b5276f3a4184
kumakuma1 <kuehnmaxi@gmail.com>
2026-06-21 11:10:18 +0200

    shorten test

--- data/battle_tests/moves/sludge_wave/chain_kills.c
+++ data/battle_tests/moves/sludge_wave/chain_kills.c
@@ -78,7 +78,7 @@ BEGIN_TEST
             .level = 5,
             .form = 0,
             .ability = ABILITY_NO_GUARD,
-            .item = ITEM_NONE,
+            .item = ITEM_FOCUS_SASH,
             .moves = { MOVE_SLEEP_TALK, MOVE_NONE, MOVE_NONE, MOVE_NONE },
             .hp = FULL_HP,
             .status = 0,
@@ -103,7 +103,7 @@ BEGIN_TEST
                           { ACTION_MOVE_SLOT_1, BATTLER_ENEMY_FIRST },
                           { ACTION_MOVE_SLOT_1, BATTLER_ENEMY_FIRST },
                           { ACTION_MOVE_SLOT_1, BATTLER_ENEMY_FIRST },
-                          { ACTION_MOVE_SLOT_1, BATTLER_ENEMY_FIRST },
+                          { ACTION_NONE, 0 },
                           { ACTION_NONE, 0 },
                           { ACTION_NONE, 0 },
                       },
@@ -123,7 +123,7 @@ BEGIN_TEST
                          { ACTION_MOVE_SLOT_1, BATTLER_PLAYER_FIRST },
                          { ACTION_MOVE_SLOT_1, BATTLER_PLAYER_FIRST },
                          { ACTION_MOVE_SLOT_1, BATTLER_PLAYER_FIRST },
-                         { ACTION_MOVE_SLOT_1, BATTLER_PLAYER_FIRST },
+                         { ACTION_NONE, 0 },
                          { ACTION_NONE, 0 },
                          { ACTION_NONE, 0 },
                      },
@@ -138,8 +138,7 @@ BEGIN_TEST
             { ACTION_NONE, 0 },
         } },
     .expectations = {
-        { .expectationType = EXPECTATION_TYPE_MESSAGE, .expectationValue.message = "The opposing Chespin fainted!" },
-        { .expectationType = EXPECTATION_TYPE_MESSAGE, .expectationValue.message = "The opposing Rowlet hung on using its Focus Sash!" },
+        { .expectationType = EXPECTATION_TYPE_MESSAGE, .expectationValue.message = "The opposing Chespin hung on using its Focus Sash!" },
     }
 }
 END_TEST
