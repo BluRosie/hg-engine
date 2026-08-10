1ba203ca23bbe8355db8dde092ba68b385068d9b
zebben <zachebben@gmail.com>
2026-08-10 13:34:27 -0500

    merge fixes

--- include/types.h
+++ include/types.h
@@ -142,6 +142,7 @@ void LONG_CALL ReadFromNarcMemberByIdPair(void *data, int narcID, int fileID, in
 
 void *LONG_CALL sys_AllocMemoryLo(u32 heap_id, u32 size);
 void *LONG_CALL NARC_New(u32 narc_id, u32 heap_id);
+void LONG_CALL NARC_Delete(void *narc);
 
 void LONG_CALL MIi_CpuClearFast(u32 value, void *dest, u32 size);
 u16 LONG_CALL GF_CalcCRC16(const void *data, u32 size);
