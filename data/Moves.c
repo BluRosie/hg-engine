06911d84524b97114d2cc116348e433da57123de
kumakuma1 <kuehnmaxi@gmail.com>
2026-09-10 12:57:36 +0200

    milk drink

--- data/Moves.c
+++ data/Moves.c
@@ -5633,7 +5633,7 @@ const MoveSourceEntry sMoveSource[NUM_OF_MOVES + 1] = {
             .effectChance = 0,
         },
         .battle = {
-            .target = RANGE_USER,
+            .target = RANGE_SINGLE_TARGET_USER_SIDE,
             .priority = 0,
             .flags = FLAG_SNATCH,
         },
