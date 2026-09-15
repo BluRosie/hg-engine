523becf29da4e4661516352586cdc9581e7ef46f
kumakuma1 <kuehnmaxi@gmail.com>
2026-09-15 10:32:39 +0200

    use correct expectation

--- data/battle_tests/abilities/bad_dreams/singles_trace.c
+++ data/battle_tests/abilities/bad_dreams/singles_trace.c
@@ -87,7 +87,7 @@ BEGIN_TEST
         { .expectationType = EXPECTATION_TYPE_MESSAGE, .expectationValue.message = "It traced the opposing Darkrai's Bad Dreams!" },
         { .expectationType = EXPECTATION_TYPE_MESSAGE, .expectationValue.message = "The opposing Darkrai used Hypnosis!" },
         { .expectationType = EXPECTATION_TYPE_MESSAGE, .expectationValue.message = "Gardevoir fell asleep!" },
-        { .expectationType = EXPECTATION_TYPE_MESSAGE_CONTAINS, .expectationValue.message = "Gardevoir" },
+        { .expectationType = EXPECTATION_TYPE_MESSAGE_DOES_NOT_CONTAIN, .expectationValue.message = "Gardevoir" },
         { .expectationType = EXPECTATION_TYPE_MESSAGE, .expectationValue.message = "Gardevoir is tormented!" },
     }
 }
